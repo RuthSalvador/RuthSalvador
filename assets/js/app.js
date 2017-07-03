5f7b88e3cda8e636c752156b551bb95456c01e75
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-07-02 20:07:08 -0500

    esquema: home, about, skills

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+/* Circulo de iconos - Evento Click */
+/*
+var iniciales = document.querySelectorAll(".alrededor a");
+for(var i = 0; i < iniciales.length; i++) {
+  iniciales[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/iniciales.length)*i*Math.PI)).toFixed(4) + "%";
+  iniciales[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/iniciales.length)*i*Math.PI)).toFixed(4) + "%";
+}
+document.querySelector(".icon-helado").addEventListener("click", function(e){
+  e.preventDefault();
+  document.querySelector(".alrededor").classList.toggle("open");
+});*/
