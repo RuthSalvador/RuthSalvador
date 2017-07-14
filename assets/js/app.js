a34bee66b3ea1e05d4bb71828df27246198c17d2
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-07-14 01:54:42 -0500

    cambios imagen y colores

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,27 @@
 
-window.addEventListener("load", function() {
+$(document).ready(function(){
+
+  /** scroll **/
   window.addEventListener("scroll", function () {
     var currentScroll = window.pageYOffset || document.body.scrollTop;
     var navHeader = document.getElementById("nav--header");
+      if (currentScroll >= 150) {
+        navHeader.classList.add("background-dark");
+        navHeader.classList.remove("background-cap");
 
-    if (currentScroll > 150) {
-      navHeader.classList.add("background-dark");
-      navHeader.classList.remove("background-cap");
-
-    } else {
-      navHeader.classList.add("background-cap");
-      navHeader.classList.remove("background-dark");
-    }
-
+      } else {
+        navHeader.classList.add("background-cap");
+        navHeader.classList.remove("background-dark");
+      }
   }, false);
+
+  /** carousel **/
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+    items:1,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+  });
 });
 
